b04d1508ea5d4b011773c009513c80ec33966405
wilder <robert.g.wilder@gmail.com>
2021-06-15 17:23:57 +0100

    named functions

--- cards/main.go
+++ cards/main.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+var deckSize int
+
 func main() {
 
+	card := newCard()
+
+	fmt.Println(card)
+
+
+}
+
+
+func newCard() string {
+	return "Five of Diamonds"
+}
 	// var card string = "Ace of Spades"
 	// Two means of using variables 
 	// only use := when you are assigning a new variable 
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	// card := "Ace of Spades"
+	// card = "Five of Diamonds"
+	// fmt.Println(card)
 
-}
+
+	// You can Initialize the variable later
+	// var deckSize int
+//   deckSize = 52
+//   fmt.Println(deckSize)
+
+// variable can be intialized out of the the func main()
+// deckSize = 50
+// fmt.Println(deckSize)
 
 // Go is a statically typed language
 
