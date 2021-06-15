b420bdc30eb2fc3a4066210113ef21286e235338
wilder <robert.g.wilder@gmail.com>
2021-06-15 17:10:48 +0100

    Learning variables

---
+++ cards/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var card string = "Ace of Spades"
+	// Two means of using variables 
+	// only use := when you are assigning a new variable 
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+	fmt.Println(card)
+
+}
+
+// Go is a statically typed language
+
+// Bool 
+// string 
+// int 
+// float64 - number with a decimal after it. 10.000001
